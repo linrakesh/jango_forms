492ec70312e02e8b22e7fb27d3971a6d2e3da524
rakesh kumar <rakesh.linux@gmail.com>
2019-04-02 06:27:57 +0530

    Working image in admin and front end + static files like logo and css are working

--- student/admin.py
+++ student/admin.py
@@ -6,10 +6,10 @@ class candidateAdmin(admin.ModelAdmin):
     list_display = ['admno','name','fname','dob','std','section','image_tag']
     search_fields = ('name','fname')
     ordering =  ('section',)
-    list_per_page = 15
+    list_per_page = 4
 
 
     def image_tag(self, obj):
-        return format_html('<img src="{}" />'.format(obj.photo.url))
+        return format_html('<img src="{}" width="50" height="50"/>'.format(obj.photo.url))  #only helpful when media and other information is set in settings.py file
 # Register your models here.
 admin.site.register(candidate,candidateAdmin)
