0ee590ce61827fa24ad69474037448ba2a02db82
rakesh <rakesh.linux@gmail.com>
2019-04-02 22:57:36 +0530

    Delete object using deleteView

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import CandidateList,CandidateCreate
-
+from .views import CandidateList,CandidateCreate, CandidateDetail, CandidateDelete
+app ="stud"
 urlpatterns = [
     path('student/', CandidateList.as_view(), name="student-list"),
     path('student-add/', CandidateCreate.as_view(), name="student-add"),
+    path('student/<int:pk>/', CandidateDetail.as_view(), name="student-detail"),
+    path('student/<int:pk>/delete', CandidateDelete.as_view(), name="student-delete"),
+
     
 ]
--- student/views.py
+++ student/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import candidate
 # Create your views here.
 
@@ -12,8 +14,19 @@ class CandidateList(ListView):
     template_name = 'student/list.html'
     paginate_by = 3
 
+class CandidateDetail(DetailView):
+    model = candidate
+    template_name = 'student/detail.html'
+
+
 class CandidateCreate(CreateView):
     model = candidate
     # fields = ('admno','name','fname','dob','gender','phone','std','section','active')
     fields = '__all__'
     template_name = 'student/add.html'
+
+
+class CandidateDelete(DeleteView):
+    model = candidate
+    success_url = reverse_lazy('student-list')
+    # it delete data on confirm as well as on cancel. 
