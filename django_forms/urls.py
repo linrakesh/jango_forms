ad0ece57a2fbfd54f7792e3bc8601dfea9a1c2bb
rakesh <rakesh.linux@gmail.com>
2019-04-11 13:45:21 +0530

    Password Reset

--- django_forms/urls.py
+++ django_forms/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.auth.views import LoginView,LogoutView,PasswordResetView,PasswordResetConfirmView
+from django.contrib.auth.views import (
+        LoginView,
+        LogoutView,
+        PasswordResetView,
+        PasswordResetDoneView,
+        PasswordResetConfirmView, 
+        PasswordResetCompleteView,
+        )
 from django.conf import settings   # this is useful for importing media settings
 from django.conf.urls.static import static  # this is for static files
 
@@ -25,6 +32,13 @@ urlpatterns = [
     path('login/',LoginView.as_view(template_name='registration/login.html'),name='login'),
     path('logout/',LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
     path('password-reset/',PasswordResetView.as_view(template_name='registration/password_reset.html'),name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>',PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset/done/',PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),name='password_reset_done'),
+    
+    path('prc/',
+            PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+            name='password_reset_complete'),
+    
     path('', include('student.urls')),
 ]
 
